3b4d03be59b217a000228b742795a8ca13089d94
KAPERA89 <othmanedarhoni0@gmail.com>
2024-03-13 21:24:36 +0000

    all

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>HomePage</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae consequuntur dicta earum! Sint modi esse nam dolores, tenetur alias ratione minima? Ducimus libero dolorum necessitatibus nemo laboriosam repellat laudantium molestias accusantium vitae labore! Est odit libero nulla a doloribus fugiat quidem sed consequuntur, atque, repellat deserunt incidunt soluta. Nesciunt doloremque sapiente dolor nihil hic tempora, rem facere praesentium officiis, modi, architecto aspernatur aliquam blanditiis? Ducimus iste mollitia tempora consectetur enim rerum nostrum perferendis at qui cumque, placeat accusantium aliquam magni iure? Cumque expedita repellat at, labore a animi odit inventore nisi odio commodi esse doloribus, tempora aliquid autem ea dolorum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae consequuntur dicta earum! Sint modi esse nam dolores, tenetur alias ratione minima? Ducimus libero dolorum necessitatibus nemo laboriosam repellat laudantium molestias accusantium vitae labore! Est odit libero nulla a doloribus fugiat quidem sed consequuntur, atque, repellat deserunt incidunt soluta. Nesciunt doloremque sapiente dolor nihil hic tempora, rem facere praesentium officiis, modi, architecto aspernatur aliquam blanditiis? Ducimus iste mollitia tempora consectetur enim rerum nostrum perferendis at qui cumque, placeat accusantium aliquam magni iure? Cumque expedita repellat at, labore a animi odit inventore nisi odio commodi esse doloribus, tempora aliquid autem ea dolorum.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1>Test Component</h1>
+
+    <button @click="a++">Increment A</button>
+    <button @click="b++">Increment B</button>
+
+
+    <p>A : {{a}}</p>
+    <p>B : {{b}}</p>
+
+    <p>age + A = {{AgeWithA}}</p>
+    <p>age + B = {{AgeWithB}}</p>
+
+</template>
+
+
+
+<script>
+export default {
+    name: 'Test',
+    data() {
+        return {
+            a: 0,
+            b:0,
+            age:20
+        }
+    },
+    computed: {
+        AgeWithA(){
+            console.log("AgeWithA called")
+            return this.age = this.a;
+        },
+        AgeWithB(){
+            console.log("AgeWithB called")
+            return this.age = this.b;
+        }
+    },
+}
+</script>
+
+
+<style>
+    
+</style>
